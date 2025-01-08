9dc36a26842a7f002aa62b690e7bc1424b6ed9e3
Elison Nayak <nayakelison@gmail.com>
2025-01-07 22:47:16 -0500

    Update

--- bootdemo3.x/src/main/java/com/example/demo/controllers/MoviesController.java
+++ bootdemo3.x/src/main/java/com/example/demo/controllers/MoviesController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/movies")
+@CrossOrigin(value = "*")
 public class MoviesController {
-
     @GetMapping(path = "/allMovies")
     public @ResponseBody List<ResponseBean> getAllMovies(){
         List<ResponseBean> allMovies = new ArrayList<ResponseBean>();
@@ -26,6 +28,11 @@ public class MoviesController {
         bean = ResponseBean.builder().id("600").title("Movie6").releaseDate("05012020").build();
         allMovies.add(bean);
         return allMovies;
+    }
+
+    @GetMapping(path = "searchMovie")
+    private @ResponseBody List<ResponseBean> getMovieDetails(@RequestParam String query){
+        return this.getAllMovies().stream().filter(movie -> movie.getTitle().equalsIgnoreCase(query)).collect(Collectors.toList());
     }
 
 }
